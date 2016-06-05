495a7873939b6a6c6cec14267dd9f6d5522d0ddb
felixum <felix_umanski@hotmail.com>
2016-06-05 13:52:06 +0300

    5/6

--- MenegerAndTeacherForm.cs
+++ MenegerAndTeacherForm.cs
@@ -343,5 +343,13 @@ namespace LearnMath
             }
         }
 
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ShowProgres uga = new ShowProgres();
+            uga.Show(); 
+
+        }
+
     }
 }
--- ShowProgres.Designer.cs
+++ ShowProgres.Designer.cs
@@ -29,96 +29,59 @@
         private void InitializeComponent()
         {
             this.Report = new System.Windows.Forms.Label();
-            this.StartDate = new System.Windows.Forms.Label();
-            this.EndDate = new System.Windows.Forms.Label();
             this.IdSerch = new System.Windows.Forms.Label();
             this.ShowReport = new System.Windows.Forms.Button();
-            this.StartDateTxt = new System.Windows.Forms.DateTimePicker();
             this.IdSerchTxt = new System.Windows.Forms.TextBox();
-            this.EndDateTxt = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Report
             // 
             this.Report.AutoSize = true;
-            this.Report.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Report.Location = new System.Drawing.Point(207, 27);
+            this.Report.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Report.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Report.Location = new System.Drawing.Point(271, 9);
             this.Report.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Report.Name = "Report";
-            this.Report.Size = new System.Drawing.Size(276, 44);
+            this.Report.Size = new System.Drawing.Size(94, 43);
             this.Report.TabIndex = 0;
-            this.Report.Text = "הצגת דוח תלמיד";
-            // 
-            // StartDate
-            // 
-            this.StartDate.AutoSize = true;
-            this.StartDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StartDate.Location = new System.Drawing.Point(554, 172);
-            this.StartDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.StartDate.Name = "StartDate";
-            this.StartDate.Size = new System.Drawing.Size(66, 20);
-            this.StartDate.TabIndex = 1;
-            this.StartDate.Text = ":מתאריך";
-            // 
-            // EndDate
-            // 
-            this.EndDate.AutoSize = true;
-            this.EndDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EndDate.Location = new System.Drawing.Point(554, 197);
-            this.EndDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.EndDate.Name = "EndDate";
-            this.EndDate.Size = new System.Drawing.Size(79, 20);
-            this.EndDate.TabIndex = 3;
-            this.EndDate.Text = ":עד תאריך";
+            this.Report.Text = "דוחות";
             // 
             // IdSerch
             // 
             this.IdSerch.AutoSize = true;
             this.IdSerch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.IdSerch.Location = new System.Drawing.Point(554, 142);
+            this.IdSerch.Location = new System.Drawing.Point(563, 119);
             this.IdSerch.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.IdSerch.Name = "IdSerch";
-            this.IdSerch.Size = new System.Drawing.Size(97, 20);
+            this.IdSerch.Size = new System.Drawing.Size(65, 20);
             this.IdSerch.TabIndex = 5;
-            this.IdSerch.Text = ":הצגה לפי ת.ז";
+            this.IdSerch.Text = "הכנס ת.ז";
             // 
             // ShowReport
             // 
             this.ShowReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ShowReport.Location = new System.Drawing.Point(183, 196);
-            this.ShowReport.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ShowReport.Location = new System.Drawing.Point(420, 207);
+            this.ShowReport.Margin = new System.Windows.Forms.Padding(2);
             this.ShowReport.Name = "ShowReport";
             this.ShowReport.Size = new System.Drawing.Size(71, 31);
             this.ShowReport.TabIndex = 7;
             this.ShowReport.Text = "הצג דוח";
             this.ShowReport.UseVisualStyleBackColor = true;
-            // 
-            // StartDateTxt
-            // 
-            this.StartDateTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StartDateTxt.Location = new System.Drawing.Point(392, 168);
-            this.StartDateTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.StartDateTxt.Name = "StartDateTxt";
-            this.StartDateTxt.Size = new System.Drawing.Size(151, 26);
-            this.StartDateTxt.TabIndex = 9;
+            this.ShowReport.Click += new System.EventHandler(this.ShowReport_Click);
             // 
             // IdSerchTxt
             // 
-            this.IdSerchTxt.Location = new System.Drawing.Point(466, 145);
-            this.IdSerchTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.IdSerchTxt.Location = new System.Drawing.Point(420, 119);
+            this.IdSerchTxt.Margin = new System.Windows.Forms.Padding(2);
             this.IdSerchTxt.Name = "IdSerchTxt";
-            this.IdSerchTxt.Size = new System.Drawing.Size(76, 20);
+            this.IdSerchTxt.Size = new System.Drawing.Size(121, 20);
             this.IdSerchTxt.TabIndex = 6;
-            // 
-            // EndDateTxt
-            // 
-            this.EndDateTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EndDateTxt.Location = new System.Drawing.Point(392, 197);
-            this.EndDateTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.EndDateTxt.Name = "EndDateTxt";
-            this.EndDateTxt.Size = new System.Drawing.Size(151, 26);
-            this.EndDateTxt.TabIndex = 10;
             // 
             // button1
             // 
@@ -133,23 +96,81 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(547, 159);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "בחר משחק";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(420, 158);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 41;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(62, 112);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 31);
+            this.button2.TabIndex = 42;
+            this.button2.Text = "הצג דוח";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(461, 69);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 31);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "הצגת דוח תלמיד";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(56, 69);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(231, 31);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "הצגת דוח כל התלמידים";
+            // 
             // ShowProgres
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(657, 410);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.EndDateTxt);
-            this.Controls.Add(this.StartDateTxt);
             this.Controls.Add(this.ShowReport);
             this.Controls.Add(this.IdSerchTxt);
             this.Controls.Add(this.IdSerch);
-            this.Controls.Add(this.EndDate);
-            this.Controls.Add(this.StartDate);
             this.Controls.Add(this.Report);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ShowProgres";
             this.Text = "ShowProgres";
+            this.Load += new System.EventHandler(this.ShowProgres_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,13 +179,14 @@
         #endregion
 
         private System.Windows.Forms.Label Report;
-        private System.Windows.Forms.Label StartDate;
-        private System.Windows.Forms.Label EndDate;
         private System.Windows.Forms.Label IdSerch;
         private System.Windows.Forms.Button ShowReport;
-        private System.Windows.Forms.DateTimePicker StartDateTxt;
         private System.Windows.Forms.TextBox IdSerchTxt;
-        private System.Windows.Forms.DateTimePicker EndDateTxt;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
